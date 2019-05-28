eaae8df6b787e632a32f55cbe5f65ceea085e508
abircic <antee_92@hotmail.com>
2019-05-28 16:13:39 +0200

    .

--- Sindikat.Ankete.API/Controllers/RezultatiController.cs
+++ Sindikat.Ankete.API/Controllers/RezultatiController.cs
@@ -48,14 +48,6 @@ namespace Sindikat.Ankete.API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<PopunjenaAnketaEntity>> GetPopunjenaAnketaEntity(int id)
         {
-            //var popunjenaAnketaEntity = await _context.PopunjeneAnkete.FindAsync(id);
-
-            //if (popunjenaAnketaEntity == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //return popunjenaAnketaEntity;
             var query = from p in _context.PopunjeneAnkete
                         where p.KorisnikId==id.ToString()
                         orderby p.KorisnikId
@@ -70,7 +62,46 @@ namespace Sindikat.Ankete.API.Controllers
             return Ok(query);
 
         }
+        //var query = _context.Ankete
+        //       .Include(anketa => anketa.Pitanja)
+        //           .ThenInclude(pitanje => pitanje.PonudeniOdgovori)
+        //           .SingleOrDefault(anketa => anketa.Id == id);
 
+
+        [HttpGet("/api/[controller]/{idAnkete}/{idPitanja}/{odgovor}")]
+        public async Task<ActionResult<PopunjenaAnketaEntity>> GetRezultatPitanja(int idAnkete, int idPitanja, string odgovor)
+        {
+            var query = from o in _context.Odgovori
+                        join p in _context.Pitanja on o.PitanjeId equals p.Id
+                        join a in _context.Ankete on p.Anketa.Id equals a.Id
+                        where o.PitanjeId==idPitanja && p.Anketa.Id==idAnkete && o.OdgovorPitanja==odgovor
+                        group o by o.OdgovorPitanja into grp
+                        select new
+                        {
+                            Odgovor = grp.Key,
+                            Broj_odgovora = grp.Count()
+
+                        };
+            
+
+            return Ok(query);
+        }
+
+        [HttpGet("/api/[controller]/BrojAnketa/{idAnkete}")]
+        public async Task<ActionResult<PopunjenaAnketaEntity>> GetUkupanBrojAnketa(int idAnkete)
+        {
+            var query = from p in _context.PopunjeneAnkete
+                        where p.AnketaId == idAnkete
+                        orderby p.AnketaId
+                        group p by p.AnketaId into grp
+                        select new
+                        {
+                            AnketaId = grp.Key,
+                            Broj_popunjenih_anketa = grp.Count()
+
+                        };
+            return Ok(query);
+        }
         // PUT: api/Rezultati/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPopunjenaAnketaEntity(int id, PopunjenaAnketaEntity popunjenaAnketaEntity)
